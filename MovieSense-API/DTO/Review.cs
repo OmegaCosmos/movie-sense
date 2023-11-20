77b3590e4bfe1e594059fba3163339810bbe6633
Oliver Chapman <33430694+OmegaCosmos@users.noreply.github.com>
2023-11-20 12:31:26 +0000

    Added EF Core Support

---
+++ MovieSense-API/DTO/Review.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieSense_API.DTO
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        [Required] public int AuthorId { get; set; }
+        [Required] public float Rating { get; set; }
+        [Required] public string? Title { get; set; }
+        [Required] public string? Description { get; set; }
+    }
+}
